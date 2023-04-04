03c87436dab72e74d20b69e722033e96a9d60bfa
Pavlo <vikkaming@gmail.com>
2023-04-04 23:21:03 +0300

    Change the algorithm to dijkstra

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,12 @@ import * as morgan from 'morgan';
 import { notNil, flatten, distanceBetween, haversine } from '../util';
 import { Airport, Route, loadAirportData, loadRouteData } from '../data';
 
+interface Node {
+  airport: Airport;
+  distanceFromSource: number;
+  previousNode?: Node;
+}
+
 export async function createApp() {
   const app = express();
 
@@ -60,64 +66,77 @@ export async function createApp() {
     if (sourceAirport === undefined || destinationAirport === undefined) {
       return res.status(404).send('No such airport, please provide a valid IATA/ICAO codes');
     }
-  
-    // Find all routes from the source airport
-    const sourceRoutes = routes.filter((route) => route.source.id === sourceAirport.id);
-    const validRoutes: Route[][] = [];
-  
-    // For each route from the source, find all possible combinations of up to maxStops flights
-    for (const sourceRoute of sourceRoutes) {
-      const hops: Route[] = [sourceRoute];
-  
-      for (let stops = 1; stops <= maxStops; stops++) {
-        const lastHop = hops[hops.length - 1];
-        const nextRoutes = routes.filter((route) => route.source.id === lastHop.destination.id);
-  
-        for (const nextRoute of nextRoutes) {
-          if (hops.some((hop) => hop.source.id === nextRoute.source.id)) {
-            // Avoid cycles
-            continue;
+    
+    const findShortestRoute = (sourceAirport: Airport, destinationAirport: Airport, maxDistance: number): Route[] => {
+      const nodes = airports.map((airport) => ({
+        airport,
+        distanceFromSource: airport.id === sourceAirport.id ? 0 : Infinity,
+        previousNode: null,
+      }));
+      const visitedNodes: Node[] = [];
+    
+      while (nodes.length > 0) {
+        // Find node with smallest distance from source
+        const currentNode = nodes.reduce((minNode, node) => node.distanceFromSource < minNode.distanceFromSource ? node : minNode);
+    
+        // Check if destination airport has been reached
+        if (currentNode.airport.id === destinationAirport.id) {
+          const route: Route[] = [];
+          let prevNode = currentNode;
+          while (prevNode.previousNode !== null) {
+            const prevAirport = prevNode.previousNode.airport;
+            const currAirport = prevNode.airport;
+            const currDistance = prevNode.distanceFromSource - prevNode.previousNode.distanceFromSource;
+            const prevNodeRoute = routes.find((r) => r.source.id === prevAirport.id && r.destination.id === currAirport.id);
+            if (!prevNodeRoute) {
+              throw new Error(`Route not found from ${prevAirport.id} to ${currAirport.id}`);
+            }
+            route.unshift(prevNodeRoute);
+            prevNode = prevNode.previousNode;
           }
-  
-          hops.push(nextRoute);
-  
-          if (nextRoute.destination.id === destinationAirport.id) {
-            // Found a valid route to the destination
-            validRoutes.push([...hops]);
-          } else if (stops < maxStops) {
-            // Keep looking for routes with more stops
-            continue;
+          return route;
+        }
+    
+        // Explore adjacent nodes
+        nodes.splice(nodes.indexOf(currentNode), 1);
+        visitedNodes.push(currentNode);
+        for (const route of routes) {
+          if (route.source.id === currentNode.airport.id) {
+            const adjacentAirport = route.destination;
+            let adjacentNode = nodes.find((node) => node.airport.id === adjacentAirport.id);
+            if (!adjacentNode) {
+              // Add new node for adjacent airport
+              const newAdjacentNode = { airport: adjacentAirport, distanceFromSource: Infinity, previousNode: null };
+              nodes.push(newAdjacentNode);
+              if (currentNode.distanceFromSource + route.distance < maxDistance) { // check maximum distance
+                adjacentNode = newAdjacentNode;
+              } else {
+                continue; // skip exploring this adjacent node
+              }
+            }
+    
+            const newDistanceFromSource = currentNode.distanceFromSource + route.distance;
+            if (newDistanceFromSource < adjacentNode.distanceFromSource) {
+              adjacentNode.distanceFromSource = newDistanceFromSource;
+              adjacentNode.previousNode = currentNode;
+            }
           }
-  
-          hops.pop();
         }
       }
-    }
-  
-    if (validRoutes.length === 0) {
-      // No valid routes found
-      return res.status(404).send('No valid routes found');
-    }
-  
-    // Find the shortest valid route
-    const shortestRoute = validRoutes.reduce((shortest, route) => {
-      const distance = route.reduce((sum, hop) => sum + hop.distance, 0);
-      return distance < shortest.distance ? { distance, route } : shortest;
-    }, { distance: Infinity, route: [] });
-  
-    const hops = shortestRoute.route.map((route) => {
-      return {
-        source: route.source.iata || route.source.icao,
-        destination: route.destination.iata || route.destination.icao,
-        distance: route.distance,
-      };
-    });
+    
+      throw new Error(`No route found from ${sourceAirport.id} to ${destinationAirport.id}`);
+    };
+
+    const shortestRoute = findShortestRoute(sourceAirport, destinationAirport, maxStops);
+    const distance = shortestRoute.reduce((sum, route) => sum + route.distance, 0);
+    const hops = shortestRoute.map((route) => route.source.iata);
+    hops.push(shortestRoute[shortestRoute.length - 1].destination.iata);
   
     return res.status(200).send({
       source: sourceAirport.iata || sourceAirport.icao,
       destination: destinationAirport.iata || destinationAirport.icao,
-      distance: shortestRoute.distance,
-      hops,
+      distance: distance,
+      hops: hops,
     });
   });
 
